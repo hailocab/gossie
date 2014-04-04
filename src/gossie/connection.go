b0e626c9ba49e1ca43bf04421faa499b02c6d32c
Matt Oddie <moddie@hailocab.com>
2014-04-04 13:53:20 +0100

    Add error back, keep retrying if we failure to acquire connection

--- src/gossie/connection.go
+++ src/gossie/connection.go
@@ -220,7 +220,7 @@ func (cp *connectionPool) runWithRetries(t transaction, retries int) error {
 			c, err = cp.acquire()
 			// nothing to do, cannot acquire a connection
 			if err != nil {
-				return err
+				continue
 			}
 		}
 
@@ -248,7 +248,7 @@ func (cp *connectionPool) runWithRetries(t transaction, retries int) error {
 				c.close()
 				c = nil
 				cp.releaseEmpty()
-				continue
+				return terr
 			}
 		}
 
